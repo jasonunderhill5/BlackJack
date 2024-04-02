c23d42e2c225817d581547e655909c42208c8d9c
jasonunderhill5 <jasonunderhill4005@gmail.com>
2024-04-02 13:03:22 -0700

    update constants and variables

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 /*-------------------------------- Constants --------------------------------*/
-const cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ]
+const cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-const suits = ["C", "D", "H", "S"]
-
+const suits = ['h', 'd', 'c', 's']
 
 
 /*-------------------------------- Variables --------------------------------*/
-let dealerScore = 0
-let yourScore = 0
-let dealerAceCount = 0
-let playerAceCount = 0
-let hidden
-let deck
-let hit = true
-deck = []
+let deck = []
+let dealerHand = []
+let playerHand = []
+let cash = 100
+let youWon = false
+let dealerWon = false
+let dealerBlackjack = false
+let playerBlackjack = false
+let busted = false
+let dealerBusted = false
+let dealerTurn = false
+let playerTurn = false
+let dealerSum = 0
+let playerSum = 0
 
 /*------------------------ Cached Element References ------------------------*/
 
