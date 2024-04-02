ada2dbe56b3594ed97de7d1376c6c4b6a2d81e8e
jasonunderhill5 <jasonunderhill4005@gmail.com>
2024-04-01 18:10:29 -0700

    organize html. adding cards by just images

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
 /*-------------------------------- Constants --------------------------------*/
+const cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ]
+
+const suits = ["C", "D", "H", "S"]
 
 
 
 /*-------------------------------- Variables --------------------------------*/
-let card, dealer, player, score, wager, cash, message
-
+let dealerScore = 0
+let yourScore = 0
+let dealerAceCount = 0
+let playerAceCount = 0
+let hidden
+let deck
+let hit = true
+deck = []
 
 /*------------------------ Cached Element References ------------------------*/
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+
 
 
 
