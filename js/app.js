a09ed70a0d7506d7b622c56080eba5e756f9e1d8
jasonunderhill5 <jasonunderhill4005@gmail.com>
2024-04-04 05:51:06 -0700

    add shuffleDeck () {}

--- js/app.js
+++ js/app.js
@@ -52,6 +52,11 @@ function buildDeck () {
     }
   }
 }
-function shuffleDeck () {}
+function shuffleDeck () {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    [deck[i], deck[j]] = [deck[j], deck[i]]
+  }
+}
 function startRound () {
   
